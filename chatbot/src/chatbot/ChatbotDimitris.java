1a3ca5b6a87a5bda1ea89680313898b56447742d
delta <delta@localhost.localdomain>
2017-10-04 14:17:23 -0400

    fixed isTriggered

--- chatbot/src/chatbot/ChatbotDimitris.java
+++ chatbot/src/chatbot/ChatbotDimitris.java
@@ -1,7 +1,6 @@
 package chatbot;
 
 public class ChatbotDimitris implements Topic {
-	private String[] keywords;
 	private String[] greetings = {"hi", "hello"};
 	private String[] finishedKeywords = {"done", "finished"};
 	private String[] positiveKeywords = {"thank", "thanks", "great", "perfect"};
@@ -19,27 +18,16 @@ public class ChatbotDimitris implements Topic {
 		this.response = ChatbotMain.getInput();
 		this.finished = this.findKeywords(this.finishedKeywords);
 		while(!this.finished) {
-			if())
-
-
 			ChatbotMain.print("have you selected all of your foods?");
 			this.finished = this.findKeywords(this.finishedKeywords) || this.findKeywords(this.positiveKeywords);
 		}
 		//acces variables from other classes
 		ChatbotMain.print("YOU HAVE SELECTED ALL YOUR INGREDIENTS :)");
-		ChatbotMain.chatbot.startChatting();
 	}
 	
 	public boolean isTriggered(String response) {
-		for(int i = 0 ; i < this.keywords.length; i++) {
-			//Important on Rubric
-			this.response = response;
-			if(this.findKeywords(this.greetings)) {
-				return true;
-			}
-		}
-		return false;
-	
+		this.response = response;
+		return this.findKeywords(this.greetings);	
 	}
 	
 	public boolean findKeywords(String[] keywords) {
