311c38865617a02e4e3d7647c25f7517e8e59d58
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2017-10-05 15:01:22 -0400

    Changed weird dialogue

--- chatbot/src/chatbot/ChatbotBen.java
+++ chatbot/src/chatbot/ChatbotBen.java
@@ -46,7 +46,7 @@ public class ChatbotBen implements Topic
 	public void talk(String response) 
 	{
 		food = info.getFoodList();
-		ChatbotMain.print("So you wanna make " + food[0].getName() + ", huh? You're gonna need to get some ingredients first. It'll cost you " + getTotalCost(food[0].getIngredients()) + ". You're also going to need some cooking tools to make it. Feel free to ask for the ingredients and tools at any time.");
+		ChatbotMain.print("So you wanna make " + food[0].getName() + ". You're gonna need to get some ingredients first. It'll cost you " + getTotalCost(food[0].getIngredients()) + ". You're also going to need some cooking tools to make it. Feel free to ask for the ingredients and tools at any time.");
 		response = ChatbotMain.getInput();
 		while(!(response.toLowerCase().equals(goodbyeKeyword))) 
 		{
