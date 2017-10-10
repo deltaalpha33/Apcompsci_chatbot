d4ea43bf0eff3334c4e809fda64aef75703fd63b
delta <delta@localhost.localdomain>
2017-10-10 15:24:36 -0400

    adde ingredient selection

--- chatbot/src/chatbot/ChatbotDimitris.java
+++ chatbot/src/chatbot/ChatbotDimitris.java
@@ -36,9 +36,29 @@ public class ChatbotDimitris implements Topic {
 			this.response = ChatbotMain.getInput();
 			int foodIndex = 0;
 			for(int i = foodIndex; i < this.response.length(); i = foodIndex) {
-				foodIndex = this.extractFood(this.response, i);
+				foodIndex = this.extractNameable((Nameable[])this.foods, this.response, i);
 				if(foodIndex == -1) {
 					ChatbotMain.print("Food was not found, do you wish to add it?");
+					this.response = ChatbotMain.getInput();
+					if(this.findKeywords(this.affirmativeKeywords)) {
+						Food newFood = new Food();
+						ChatbotMain.print("Food Name:");
+						newFood.setName(ChatbotMain.getInput());
+						
+						ChatbotMain.print("Nessisary Ingredients (separate with commas):");
+						this.response = ChatbotMain.getInput();
+						for(int wordStart = 0, j = 0; i <= this.response.length(); j++) {
+							if(i == this.response.length()-1 || this.response.charAt(i) == ',') //short curcuits operator
+								ChatbotMain.print("How much does it cost?");
+								float ingredientCost = Float.parseFloat(ChatbotMain.getInput());
+								this.addIngredient(new Ingredient(this.response.substring(wordStart, i), ingredientCost), newFood);
+								wordStart = i;
+							
+						}
+						newFood.setIngredients(ingredients);
+						
+						
+					}
 					
 					break;
 				}
@@ -71,9 +91,9 @@ public class ChatbotDimitris implements Topic {
 		return false;
 	}
 	
-	public int extractFood(String str, int startPos) {
+	public int extractNameable(Nameable[] nameableList, String str, int startPos) {
 		for(int i = 0; i < chatbot.getFoodList().length; i++) {
-			if(ChatbotMain.keywordIsIsolated(startPos, chatbot.getFoodList()[i].getName(), str)){
+			if(ChatbotMain.keywordIsIsolated(startPos, nameableList[i].getName(), str)){
 				return i;
 			}
 		}
@@ -83,6 +103,12 @@ public class ChatbotDimitris implements Topic {
 	public void addSelectedFood(int foodIndex) {
 		this.selectedFoods = Arrays.copyOf(this.selectedFoods,this.selectedFoods.length+1);
 		this.selectedFoods[this.selectedFoods.length] = this.foods[foodIndex];
+	}
+	
+	public void addIngredient(Ingredient ingredient, Food food) {
+		Ingredient[] addArray = Arrays.copyOf(food.getIngredients(),food.getIngredients().length+1);
+		addArray[addArray.length] = ingredient;
+		food.setIngredients(addArray);
 	}
 	
 
